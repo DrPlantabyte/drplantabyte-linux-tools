fef3c344d144b304531d6ec6fb657423367bf8d8
Chris Hall <explosivegnome@yahoo.com>
2023-10-26 08:22:29 +0800

    better copy-paste

---
+++ $HOME/.local/profile.d/ctl+c_copy_ctrl+x_inter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script changes the terminal bindings so that you can set ctrl+C and
+# ctrl+V to copy and paste respectively
+# Note: process interrupt is rebound to ctrl+X and escaped char (aka lnext) is 
+# rebound to ctrl+L
+# use `stty -a` to see current settings and `stty -g` to generate stty hex
+# code, and `stty <hex code>` to load complete stty settings
+# Note: binding anything to ^I breaks tab completion, reason unknown
+
+stty intr ^X lnext ^L
+
