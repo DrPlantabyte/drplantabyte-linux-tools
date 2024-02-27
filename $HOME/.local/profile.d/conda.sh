dbbd91a36e2cbdf0ae087f79f11bb997e004d849
Chris Hall <explosivegnome@yahoo.com>
2024-02-27 13:13:54 +0800

    fix for not hainv a conda env

--- $HOME/.local/profile.d/conda.sh
+++ $HOME/.local/profile.d/conda.sh
@@ -10,20 +10,22 @@ elif [ -d "$ANACONDA_PATH" ] ; then
 	CONDA_PATH="$ANACONDA_PATH"
 fi
 
-# >>> conda initialize >>>
-__conda_setup="$($CONDA_PATH/bin/conda "shell.$(basename "$SHELL")" 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
-        . "$CONDA_PATH/etc/profile.d/conda.sh"
-    else
-        export PATH="$CONDA_PATH/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
+if [ -d "$CONDA_PATH" ] ; then
+	# >>> conda initialize >>>
+	__conda_setup="$($CONDA_PATH/bin/conda "shell.$(basename "$SHELL")" 'hook' 2> /dev/null)"
+	if [ $? -eq 0 ]; then
+		eval "$__conda_setup"
+	else
+		if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
+		. "$CONDA_PATH/etc/profile.d/conda.sh"
+		else
+		export PATH="$CONDA_PATH/bin:$PATH"
+		fi
+	fi
+	unset __conda_setup
+	# <<< conda initialize <<<
 
-conda deactivate
-conda activate "$DEFAULT_ENV"
+	conda deactivate
+	conda activate "$DEFAULT_ENV"
+fi
 
